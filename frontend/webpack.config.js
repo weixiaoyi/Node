7d498af95da6006d59bc658b255bcb37ebf5bd46
weixiaoyi <2278095567@qq.com>
2017-12-04 23:20:34 +0800

    webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: './wei_xiaoyi.com/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+
+    ]
+  },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin()
   ]
 };
