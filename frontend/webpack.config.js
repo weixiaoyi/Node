bc6890bda8a416952e0f09ec3b440fa28a84d597
weixiaoyi <2278095567@qq.com>
2017-12-05 23:00:57 +0800

    webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack'); // 用于访问内置插件
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry:'./wei_xiaoyi.com/index.js',
+  devtool: 'inline-source-map',
   output: {
-    filename: '[name].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist/wei_xiaoyi.com/')
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
-    compress: true,
+    hot: true,
     port: 9000
   },
   module: {
@@ -23,14 +25,12 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
+        use: ['style-loader','css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
         use: [
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          }
+          'file-loader'
         ]
       }
     ]
@@ -39,6 +39,9 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title:'前端主页'
-    })
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+    // new UglifyJSPlugin()
   ]
 };
--- frontend/wei_xiaoyi.com/index.js
+++ frontend/wei_xiaoyi.com/index.js
@@ -1,4 +1,14 @@
 import './index.css'
+import Icon from './image/01-find.png'
 // let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
 // console.log(y)
+function component() {
+  var element = document.createElement('div')
+  var myIcon = new Image()
+  myIcon.src = Icon
+  element.appendChild(myIcon)
+  return element;
+}
 document.write('<h1 style="font-size: 1rem">前端主页</h1>')
+document.body.appendChild(component());
+
