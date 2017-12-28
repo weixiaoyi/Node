f87864166504f6a800db81d9e9667f2a8acd1d85
weixiaoyi <2278095567@qq.com>
2017-12-28 23:40:21 +0800

    https证书

--- backend/wei_xiaoyi.com/controllers/office.js
+++ backend/wei_xiaoyi.com/controllers/office.js
@@ -3,15 +3,15 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/userlist', (req, res, next)=>{
-   const pagesize=10;
-   let totalnum;
+   const pagesize=10
+   let totalnum
    const currentpage=parseInt(req.query.page)
    let Data=[]
    for(let i=0;i<500;i++){
       Data.push({
          id: i,
          name: '我是'+i,
-         phonenumber: '111-111-11111',
+         phonenumber: '111-111-1111',
          status: '待审核',
          timeregister: '2017.8.13',
          timeactive: '2017.9.13'
@@ -25,15 +25,15 @@ router.get('/userlist', (req, res, next)=>{
 })
 
 router.get('/coachlist', (req, res, next)=>{
-   const pagesize=10;
-   let totalnum;
+   const pagesize=10
+   let totalnum
    const currentpage=parseInt(req.query.page)
    let Data=[]
    for(let i=0;i<500;i++){
       Data.push({
          id: i,
          name: '我是'+i,
-         phonenumber: '111-111-11111',
+         phonenumber: '111-111-1111',
          status: '待审核',
          timeregister: '2017.8.13',
          timeactive: '2017.9.13'
@@ -43,6 +43,15 @@ router.get('/coachlist', (req, res, next)=>{
    totalnum=Data.length
    res.json({
       totalnum,pagesize,currentpage,tableData
+   })
+})
+
+router.get('/coachinfo', (req, res, next)=>{
+   res.json({
+      basicInfo:{name:req.query.id+'Name',sex:'男',phoneNum:'183-532-68994',cert:421182199209191750,birthday:'1992.09.19'},
+      careerAbility:{careertime:'3年零6个月',gyn:'深海花园健身吧',gynaddress:'深海人民大道7号'},
+      degreeInfo:{maxscholl:'本科',graduatefrom:'北京大学',major:'体育',graduatetime:'2016.12.13'},
+
    })
 })
 
