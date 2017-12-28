ed9b335a4f77648c9b7a258f69a9563debad16f1
weixiaoyi <2278095567@>
2017-12-28 18:16:58 +0800

    辅助office

--- backend/wei_xiaoyi.com/controllers/office.js
+++ backend/wei_xiaoyi.com/controllers/office.js
@@ -11,7 +11,29 @@ router.get('/userlist', (req, res, next)=>{
       Data.push({
          id: i,
          name: '我是'+i,
-         phonenumber: '183-532-68994',
+         phonenumber: '111-111-11111',
+         status: '待审核',
+         timeregister: '2017.8.13',
+         timeactive: '2017.9.13'
+      })
+   }
+   const tableData=Data.slice((currentpage-1)*pagesize,currentpage*pagesize)
+   totalnum=Data.length
+   res.json({
+      totalnum,pagesize,currentpage,tableData
+   })
+})
+
+router.get('/coachlist', (req, res, next)=>{
+   const pagesize=10;
+   let totalnum;
+   const currentpage=parseInt(req.query.page)
+   let Data=[]
+   for(let i=0;i<500;i++){
+      Data.push({
+         id: i,
+         name: '我是'+i,
+         phonenumber: '111-111-11111',
          status: '待审核',
          timeregister: '2017.8.13',
          timeactive: '2017.9.13'
